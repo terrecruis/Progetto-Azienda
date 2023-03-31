854ad47e6b005b2edd983e04410bee8fd59ba4c9
Francesco Terrecuso <terrecuso.francesco@libero.it>
2023-03-31 16:50:28 +0200

    schema fisico

--- SCHEMA FISICO/tabelle.sql
+++ SCHEMA FISICO/tabelle.sql
@@ -1,54 +1,93 @@
+/*
+	Questa parte è inerente al DDL : Data Definition Language 
+	in cui creaimo lo Schema, le tabelle, domini, viste, e i vincoli (di tabelle):
+	1. Vincoli su valori NULL e valori predefiniti
+	2. Vincoli sulle tuple (CHECK)
+	3. vincoli sui domini degli attributi
+	4. Vincoli di chiave
+	5. Vincoli di integrità referenziale
+*/
+
+
+
+CREATE DOMAIN DOMINIO_IMPIEGATO AS VARCHAR CHECK(VALUE IN('junior','middle','senior'));
+CREATE DOMAIN DOMINIO_CONTRATTO AS VARCHAR CHECK(VALUE IN('determinato','indeterminato'));
 
 
 CREATE TABLE IF NOT EXISTS IMPIEGATO
 (
-	matricola VARCHAR PRIMARY KEY,
-	nome CHAR NOT NULL,
-	cognome CHAR NOT NULL,
-	codice_fiscale VARCHAR NOT NULL UNIQUE,
-	curriculum VARCHAR,
-	stipendio DECIMAL NOT NULL,
+	matricola VARCHAR,
+	nome VARCHAR NOT NULL,
+	cognome VARCHAR NOT NULL,
+	codice_fiscale CHAR(16) NOT NULL UNIQUE,
+	curriculum VARCHAR, 
+	stipendio DECIMAL(12,2) NOT NULL,
 	sesso CHAR NOT NULL,
-	path_foto VARCHAR,/** in alternativa uso il tipo blob ma è menu pulito come metodo */
-	tipo_impiegato CHAR NOT NULL,
-	isDirigente BOOLEAN NOT NULL,
+	foto BYTEA,
+	tipo_impiegato DOMINIO_IMPIEGATO NOT NULL,
+	dirigente BOOLEAN NOT NULL,
+
+	CONSTRAINT impiegato_pk PRIMARY KEY(matricola),
+	CONSTRAINT sesso_corretto CHECK(sesso = 'M' OR sesso = 'F')
 );
 
 CREATE TABLE IF NOT EXISTS LABORATORIO
 (
-	id_laboratorio VARCHAR PRIMARY KEY,
-	topic CHAR NOT NULL,
+	id_laboratorio VARCHAR,
+	topic VARCHAR NOT NULL,
 	indirizzo VARCHAR NOT NULL,
-	numero_telefono INTEGER NOT NULL,
-	CONSTRAINT fk_responsabile_scientifico FOREIGN KEY(mat_responsabile) REFERENCES IMPIEGATO(matricola) 
+	numero_telefono VARCHAR NOT NULL,
+	numero_afferenti INTEGER DEFAULT 0,
+	mat_responsabile VARCHAR NOT NULL,
+
+	CONSTRAINT responsabile_scientifico_fk FOREIGN KEY(mat_responsabile) REFERENCES IMPIEGATO(matricola),
+	CONSTRAINT id_laboratorio_pk PRIMARY KEY(id_laboratorio)
 );
 
 CREATE TABLE IF NOT EXISTS PROGETTO
-{
-	cup VARCHAR PRIMARY KEY,
+(
+	cup VARCHAR,
 	nome_progetto VARCHAR UNIQUE NOT NULL,
-	budget DECIMAL NOT NULL,
-	CONSTRAINT fk_responsabile_progetto FOREIGN KEY(mat_responsabile_progetto) REFERENCES IMPIEGATO(matricola) 
+	budget DECIMAL(12,2) NOT NULL,
+	data_inizio DATE NOT NULL,
+	data_fine DATE,  				--poichè data_fine non è immediatamente decisa al momento della creazione--
+	mat_dirigente VARCHAR NOT NULL,
+	mat_referente VARCHAR NOT NULL,
 
-};
+	CONSTRAINT cup_pk PRIMARY KEY(cup),
+	CONSTRAINT responsabile_progetto_fk FOREIGN KEY(mat_dirigente) REFERENCES IMPIEGATO(matricola),
+	CONSTRAINT referente_progetto_fk FOREIGN key(mat_referente)    REFERENCES IMPIEGATO(matricola)
+);
 
 CREATE TABLE IF NOT EXISTS STORICO
 (
-	ruolo_prec VARCHAR NOT NULL,
-	nuovo_ruolo VARCHAR NOT NULL,
+	ruolo_prec DOMINIO_IMPIEGATO,
+	nuovo_ruolo DOMINIO_IMPIEGATO NOT NULL,
 	data_scatto DATE NOT NULL,
+	mat_impiegato VARCHAR,
+
+	CONSTRAINT storico_pk PRIMARY KEY(nuovo_ruolo, mat_impiegato),
+	CONSTRAINT mat_impiegato_fk FOREIGN KEY(mat_impiegato) REFERENCES IMPIEGATO(matricola)
 );
 
 CREATE TABLE IF NOT EXISTS AFFERENZA
 (
 	ore_giornaliere INTEGER NOT NULL,
-	tipo_contrattp VARCHAR NOT NULL,
-	CONSTRAINT fk_afferenza_impiegato FOREIGN KEY(mat_impiegato) REFERENCES IMPIEGATO(matricola) 
-	CONSTRAINT fk_afferenza_laboratorio FOREIGN KEY(id_lab) REFERENCES LABORATORIO(id_laboratorio)
+	tipo_contratto DOMINIO_CONTRATTO NOT NULL,
+	mat_impiegato VARCHAR NOT NULL,
+	id_lab VARCHAR NOT NULL,
+
+	CONSTRAINT mat_impiegato_afferenza_pk PRIMARY KEY(mat_impiegato),
+	CONSTRAINT impiegato_afferenza_fk FOREIGN KEY(mat_impiegato) REFERENCES IMPIEGATO(matricola),
+	CONSTRAINT afferenza_laboratorio_fk FOREIGN KEY(id_lab) REFERENCES LABORATORIO(id_laboratorio)
 );
 
 CREATE TABLE IF NOT EXISTS GESTIONE
 (
-	CONSTRAINT fk_gestione_progetto FOREIGN KEY(cup_prog) REFERENCES PROGETTO(cup) 
-	CONSTRAINT fk_gestione_laboratorio FOREIGN KEY(id_labb) REFERENCES LABORATORIO(id_laboratorio)
+	cup_prog VARCHAR NOT NULL,
+	id_lab   VARCHAR NOT NULL,
+
+	CONSTRAINT cup_id_pk PRIMARY KEY(cup_prog, id_lab),
+	CONSTRAINT gestione_progetto_pk FOREIGN KEY(cup_prog) REFERENCES PROGETTO(cup),
+	CONSTRAINT gestione_laboratorio_pk FOREIGN KEY(id_lab) REFERENCES LABORATORIO(id_laboratorio)
 );
