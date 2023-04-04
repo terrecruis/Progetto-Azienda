aec2f036c5468f8c0bcaa9161f836d0706024cd6
Francesco Terrecuso <terrecuso.francesco@libero.it>
2023-04-04 12:39:27 +0200

    on delete/on update

--- SCHEMA FISICO/tabelle.sql
+++ SCHEMA FISICO/tabelle.sql
@@ -41,7 +41,8 @@ CREATE TABLE IF NOT EXISTS LABORATORIO
 	numero_afferenti INTEGER DEFAULT 0,
 	mat_responsabile VARCHAR NOT NULL,
 
-	CONSTRAINT responsabile_scientifico_fk FOREIGN KEY(mat_responsabile) REFERENCES IMPIEGATO(matricola),
+	CONSTRAINT responsabile_scientifico_fk FOREIGN KEY(mat_responsabile) REFERENCES IMPIEGATO(matricola)
+		ON UPDATE CASCADE,
 	CONSTRAINT id_laboratorio_pk PRIMARY KEY(id_laboratorio)
 );
 
@@ -56,8 +57,11 @@ CREATE TABLE IF NOT EXISTS PROGETTO
 	mat_referente VARCHAR NOT NULL,
 
 	CONSTRAINT cup_pk PRIMARY KEY(cup),
-	CONSTRAINT responsabile_progetto_fk FOREIGN KEY(mat_dirigente) REFERENCES IMPIEGATO(matricola),
+	CONSTRAINT responsabile_progetto_fk FOREIGN KEY(mat_dirigente) REFERENCES IMPIEGATO(matricola)
+		ON UPDATE CASCADE,
+
 	CONSTRAINT referente_progetto_fk FOREIGN key(mat_referente)    REFERENCES IMPIEGATO(matricola)
+		ON UPDATE CASCADE
 );
 
 CREATE TABLE IF NOT EXISTS STORICO
@@ -69,6 +73,7 @@ CREATE TABLE IF NOT EXISTS STORICO
 
 	CONSTRAINT storico_pk PRIMARY KEY(nuovo_ruolo, mat_impiegato),
 	CONSTRAINT mat_impiegato_fk FOREIGN KEY(mat_impiegato) REFERENCES IMPIEGATO(matricola)
+		ON DELETE CASCADE ON UPDATE CASCADE
 );
 
 CREATE TABLE IF NOT EXISTS AFFERENZA
@@ -79,8 +84,10 @@ CREATE TABLE IF NOT EXISTS AFFERENZA
 	id_lab VARCHAR NOT NULL,
 
 	CONSTRAINT mat_impiegato_afferenza_pk PRIMARY KEY(mat_impiegato),
-	CONSTRAINT impiegato_afferenza_fk FOREIGN KEY(mat_impiegato) REFERENCES IMPIEGATO(matricola),
+	CONSTRAINT impiegato_afferenza_fk FOREIGN KEY(mat_impiegato) REFERENCES IMPIEGATO(matricola)
+		ON DELETE CASCADE 	ON UPDATE CASCADE,
 	CONSTRAINT afferenza_laboratorio_fk FOREIGN KEY(id_lab) REFERENCES LABORATORIO(id_laboratorio)
+		ON DELETE CASCADE	ON UPDATE CASCADE
 );
 
 CREATE TABLE IF NOT EXISTS GESTIONE
@@ -89,8 +96,10 @@ CREATE TABLE IF NOT EXISTS GESTIONE
 	id_lab   VARCHAR NOT NULL,
 
 	CONSTRAINT cup_id_pk PRIMARY KEY(cup_prog, id_lab),
-	CONSTRAINT gestione_progetto_pk FOREIGN KEY(cup_prog) REFERENCES PROGETTO(cup),
+	CONSTRAINT gestione_progetto_pk FOREIGN KEY(cup_prog) REFERENCES PROGETTO(cup)
+		ON UPDATE CASCADE	ON DELETE CASCADE,
 	CONSTRAINT gestione_laboratorio_pk FOREIGN KEY(id_lab) REFERENCES LABORATORIO(id_laboratorio)
+		ON UPDATE CASCADE	ON DELETE CASCADE
 );
 
 /*Creazione di almeno DUE VIEW :
